d2b518b04014ce6552ae816bbd80beb0ea134466
link-short <dora.family228@gmail.com>
2026-01-30 13:02:32 +0500

    Create app-list.js

---
+++ app-list.js
@@ -0,0 +1,19 @@
+const apps = [
+  { name: "Telegram", icon: "tg.png", link: "tg.exe" },
+  { name: "Discord", icon: "ds.png", link: "ds.exe" }
+];
+
+const list = document.getElementById('app-list');
+
+apps.forEach(app => {
+  const li = document.createElement('li');
+  li.className = 'app-card';
+  li.innerHTML = `
+    <img src="${app.icon}" class="app-icon">
+    <div class="app-info">
+      <h3 class="app-title">${app.name}</h3>
+      <a href="${app.link}" download class="download-btn">Скачать</a>
+    </div>
+  `;
+  list.appendChild(li);
+});
